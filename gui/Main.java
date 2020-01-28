5a21fc957e75423796e35dc20e61ae71495d2b33
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2020-01-29 01:05:06 +0900

    EditCardList

--- gui/Main.java
+++ gui/Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String args[])
     {
         HomeGUI homeGUI = new HomeGUI();
+	EditCardListGUI editCardListGUI = new EditCardListGUI("Chinese");
     }
 }
